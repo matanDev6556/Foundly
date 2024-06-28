21ff1db9e7202b956b943786f7a2d1592719e827
matan kalifa <matandev6556@gmail.com>
2024-06-28 17:52:41 +0300

    Folders

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,13 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Editii <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
+          rel="noogpener noreferrer"
         >
           Learn React
         </a>
