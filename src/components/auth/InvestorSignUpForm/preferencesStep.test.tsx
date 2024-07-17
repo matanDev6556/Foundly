1a1fd32ea632a2a8a39ecb69b433a5c19864a867
matan kalifa <matandev6556@gmail.com>
2024-07-17 16:48:37 +0300

    add some coments

--- src/components/auth/InvestorSignUpForm/preferencesStep.test.tsx
+++ src/components/auth/InvestorSignUpForm/preferencesStep.test.tsx
@@ -3,7 +3,8 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "../../../App";
 
-test("sign up basic user data correct", () => {
+// test
+test('sign up basic user data correct', () => {
   render(<App />);
   const linkElement = screen.getByTestId("check");
   expect(linkElement).toBeInTheDocument();
