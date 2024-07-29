c987c743b0daba4d1f68f64e9941cd5a4ae8626f
matan kalifa <matandev6556@gmail.com>
2024-07-29 20:56:14 +0300

    add functions utils file for clean code

--- src/components/cummon/invest-card/InvestmentCard.tsx
+++ src/components/cummon/invest-card/InvestmentCard.tsx
@@ -6,7 +6,12 @@ import InvestmentProgress from './InvestProgress';
 import 'react-toastify/dist/ReactToastify.css';
 import LikeButton from './LikeButton';
 import Company from '../../../models/Company';
-import { differenceInDays, differenceInMonths } from 'date-fns';
+
+import {
+  calculateRemainingDays,
+  formatRemainingTime,
+  formatTargetAmount,
+} from '../../../utils/functions';
 
 interface InvestmentCardProps {
   company: Company;
@@ -15,40 +20,7 @@ interface InvestmentCardProps {
 const InvestmentCard: React.FC<InvestmentCardProps> = ({ company }) => {
   const { companyDetails, raiseDetails } = company;
 
-  const formatTargetAmount = (amount: number): string => {
-    if (amount >= 1000000) {
-      return (amount / 1000000).toFixed(0) + 'M';
-    } else if (amount >= 1000) {
-      return (amount / 1000).toFixed(0) + 'K';
-    } else {
-      return amount.toString();
-    }
-  };
-
-  const calculateRemainingDays = (): number => {
-    const now = new Date();
-    const deadlineDate = raiseDetails.deadline.toDate();
-    return Math.max(0, differenceInDays(deadlineDate, now));
-  };
-
-  const formatRemainingTime = (
-    days: number
-  ): { value: string; unit: string } => {
-    if (days >= 365) {
-      const years = Math.floor(days / 365);
-      return { value: years.toString(), unit: years === 1 ? 'שנה' : 'שנים' };
-    } else if (days >= 30) {
-      const months = Math.floor(days / 30);
-      return {
-        value: months.toString(),
-        unit: months === 1 ? 'חודש' : 'חודשים',
-      };
-    } else {
-      return { value: days.toString(), unit: days === 1 ? 'יום' : 'ימים' };
-    }
-  };
-
-  const remainingDays = calculateRemainingDays();
+  const remainingDays = calculateRemainingDays(raiseDetails.deadline);
   const { value: remainingTimeValue, unit: remainingTimeUnit } =
     formatRemainingTime(remainingDays);
 
@@ -70,7 +42,7 @@ const InvestmentCard: React.FC<InvestmentCardProps> = ({ company }) => {
         />
         <InvestmentInfoItem
           value={remainingTimeValue}
-          label={`${remainingTimeUnit}נותרו `}
+          label={`${remainingTimeUnit} נותרו `}
         />
         <InvestmentInfoItem value={companyDetails.category} label="קטגוריה" />
       </div>
